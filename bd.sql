b60747106e4236c98f0a75a9816ab5aae4cc4151
Said Talge <saidtpereira@gmail.com>
2021-12-23 17:29:31 -0300

    atualização do banco de dados

--- bd.sql
+++ bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 23-Dez-2021 às 19:18
+-- Tempo de geração: 23-Dez-2021 às 20:28
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.3.21
 
@@ -42,6 +42,13 @@ CREATE TABLE IF NOT EXISTS `clientes` (
   `bairro` varchar(200) DEFAULT NULL,
   `cidade` varchar(200) DEFAULT NULL,
   `uf` varchar(2) DEFAULT NULL,
+  `renda` float(10,2) DEFAULT NULL,
+  `patrimonio` float(10,2) DEFAULT NULL,
+  `nat_ocupacao` varchar(35) DEFAULT NULL,
+  `tempo_empresa` varchar(30) DEFAULT NULL,
+  `banco` varchar(150) DEFAULT NULL,
+  `agencia` varchar(10) DEFAULT NULL,
+  `conta` varchar(15) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
@@ -59,6 +66,7 @@ CREATE TABLE IF NOT EXISTS `contratos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `arquivo` timestamp NULL DEFAULT NULL COMMENT 'caminho do arquivo',
   `tipo` int(11) DEFAULT NULL COMMENT '1 = contrato\r\n2 = contrato financeiro\r\n3 = Visita\r\n4 = Termo Ciencia\r\n5 - Procuração\r\n6 - projeto\r\n7 - protocolo\r\n8 - protocolo\r\n9 - NF\r\n10 - prestado/recebido\r\nConsultar direito os arquivos',
+  `proposta_id` int(11) DEFAULT NULL,
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
@@ -155,9 +163,28 @@ CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
 DROP TABLE IF EXISTS `propostas`;
 CREATE TABLE IF NOT EXISTS `propostas` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `cliente_id` int(11) DEFAULT NULL,
+  `etapa` int(11) DEFAULT '1' COMMENT '1=proposta apresentada\r\n2 = negociada\r\n3= financiamento/pagamento\r\n4= contratos solicitados\r\n5 = viabilidade técnica\r\n6 = projeto concluído\r\n7 = kit enviado\r\n8 = instalação realizada',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `deleted_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `proposta_historico`
+--
+
+DROP TABLE IF EXISTS `proposta_historico`;
+CREATE TABLE IF NOT EXISTS `proposta_historico` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descricao` text,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `deleted_at` datetime DEFAULT NULL,
+  `proposta_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
@@ -186,7 +213,8 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
 ('u2iumrJpZHvmQ1V802cz2IvFOD90Ztw77jBEf6RM', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62', 'YTozOntzOjY6Il90b2tlbiI7czo0MDoiWkM2OWlRbk5RZGxCY2ZSc0ZvaFdkYkQzU05KVVBmQ2NtTGlsSEVHMiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzA6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9yZWdpc3RlciI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fX0=', 1640266876),
-('S7USuQrzLzjMQnTcjRQCl65a1ykIscRxw3zcem73', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiQVloZHhtRTlyM3BnT1pob2Zra0tpN2F2VXhON016YkQ0TzNHTmd2QiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTAkekovYy9rZkJjWmdYVm5pbVlMeDloLlNHRlBkdGFyaDBBZG5EY21GdWhIUDdzZXN0NHplczYiO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJHpKL2Mva2ZCY1pnWFZuaW1ZTHg5aC5TR0ZQZHRhcmgwQWRuRGNtRnVoSFA3c2VzdDR6ZXM2Ijt9', 1640281370);
+('S7USuQrzLzjMQnTcjRQCl65a1ykIscRxw3zcem73', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiQVloZHhtRTlyM3BnT1pob2Zra0tpN2F2VXhON016YkQ0TzNHTmd2QiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly9sb2NhbGhvc3Q6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjUwOiJsb2dpbl93ZWJfNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO3M6MTc6InBhc3N3b3JkX2hhc2hfd2ViIjtzOjYwOiIkMnkkMTAkekovYy9rZkJjWmdYVm5pbVlMeDloLlNHRlBkdGFyaDBBZG5EY21GdWhIUDdzZXN0NHplczYiO3M6MjE6InBhc3N3b3JkX2hhc2hfc2FuY3R1bSI7czo2MDoiJDJ5JDEwJHpKL2Mva2ZCY1pnWFZuaW1ZTHg5aC5TR0ZQZHRhcmgwQWRuRGNtRnVoSFA3c2VzdDR6ZXM2Ijt9', 1640281370),
+('JkA7FSgRhX56nSw85FkDsGjJevdFr7CVhm2muBoU', 1, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 Edg/96.0.1054.62', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiZUlEUkczMlZQbTdUT2RTUjJHREt2TG4zMkpUclp5ZXBReXAwbE5YSCI7czozOiJ1cmwiO2E6MDp7fXM6OToiX3ByZXZpb3VzIjthOjE6e3M6MzoidXJsIjtzOjM0OiJodHRwOi8vbG9jYWxob3N0OjgwMDAvdXNlci9wcm9maWxlIjt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTtzOjE3OiJwYXNzd29yZF9oYXNoX3dlYiI7czo2MDoiJDJ5JDEwJHpKL2Mva2ZCY1pnWFZuaW1ZTHg5aC5TR0ZQZHRhcmgwQWRuRGNtRnVoSFA3c2VzdDR6ZXM2Ijt9', 1640290832);
 
 -- --------------------------------------------------------
 
@@ -206,6 +234,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `current_team_id` bigint(20) UNSIGNED DEFAULT NULL,
   `profile_photo_path` varchar(2048) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` int(11) DEFAULT NULL COMMENT '0=administrador;1=consultor',
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -216,8 +245,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Extraindo dados da tabela `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(1, 'said', 'admin@localhost', NULL, '$2y$10$zJ/c/kfBcZgXVnimYLx9h.SGFPdtarh0AdnDcmFuhHP7sest4zes6', NULL, NULL, NULL, NULL, NULL, '2021-12-23 20:42:50', '2021-12-23 20:42:50');
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `current_team_id`, `profile_photo_path`, `type`, `created_at`, `updated_at`) VALUES
+(1, 'said', 'admin@localhost', NULL, '$2y$10$zJ/c/kfBcZgXVnimYLx9h.SGFPdtarh0AdnDcmFuhHP7sest4zes6', NULL, NULL, NULL, NULL, NULL, 0, '2021-12-23 20:42:50', '2021-12-23 20:42:50');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
