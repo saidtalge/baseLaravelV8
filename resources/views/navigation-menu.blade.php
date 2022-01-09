13e520f2e50682c354bcc1b3793cfe5719ea9898
Ysaid talge <saidtalge@outlook.com>
2022-01-09 12:31:29 -0300

    implementando a pagina de usuários

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,9 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+                    <x-jet-nav-link href="{{ url('/users') }}" :active="request()->routeIs('users')">
+                        {{ __('Usuários') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
