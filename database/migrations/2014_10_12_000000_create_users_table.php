07cf3860171268a1a73bc30063f34a5695b83175
Ysaid talge <saidtalge@outlook.com>
2022-01-09 20:17:47 -0300

    criação da tabela de perfil e das colunas perfil_id e status

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
+            $table->integer('perfil_id')->default(1)->comment('0=administrador;1=usuarionormal');
+            $table->integer('status')->default(1)->comment('0=não;1=sim');
             $table->string('profile_photo_path', 2048)->nullable();
             $table->timestamps();
         });
